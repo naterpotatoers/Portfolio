ede8e88e7e6b56327ba1d36f3514b14a2ffac5ea
naterpotatoer <nate.garza@hotmail.com>
2024-02-09 22:09:59 -0800

    :sparkles: adds initial pictures of robo team, adds more volunteer stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,30 @@
+import me from './assets/robotics-team-pic-2022-me.png'
+import coders from './assets/robotics-team-pic-2022-coders.png'
+import full from './assets/robotics-team-2022-full.png'
 function App() {
   return (
-   <div>Hello World</div>
+   <div>
+    <h1>Nathanael Garza</h1>
+    <p>Always building, always learning, always coding.</p>
+
+    <h2>Volunteering</h2>
+    <h3>SJSU Robotics</h3>
+    <img src={me} alt="Only Me in Robotics Team" />
+    During Covid I was the only person on the team who was able to program the robot. I spent a lot of time being mentored by a prior president of the club. A lot of the skills I learned from him greatly shaped me to be the programmer I am today and helped me to be able to teach others how to program the robot.
+    <img src={coders} alt="Only Coders in Robotics Team" />
+    Once school opened up, I took on as many recruits as possible to help with the programming of the robot. I interviewed and trained up over 20 people that first year and have probably trained up over 40 people in total since then.
+    <img src={full} alt="Full Robotics Team" />
+    Some of the work I did for this club is still some of the most fondest things I've worked on to date. I made many friends and learned a ton not only about programming but also about leadership and how to work with others effectively.
+    
+    <h3>Cube3</h3>
+    <h3>Davis Robotics</h3>
+    <h3>HackDavis</h3>
+    <h3>Progressive Victory</h3>
+
+    <h2>Projects</h2>
+    <p>Project 1</p>
+    <p>Project 2</p>
+   </div>
   )
 }
 
