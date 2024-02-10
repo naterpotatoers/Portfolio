7c9ebec83b9c69836f6010e8573a91b6b7a5b2f8
naterpotatoer <nate.garza@hotmail.com>
2024-02-09 23:09:21 -0800

    :sparkles: create robotics asset folder, adds rough desc to all volunteer sections, adds in books sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import me from './assets/robotics-team-pic-2022-me.png'
-import coders from './assets/robotics-team-pic-2022-coders.png'
-import full from './assets/robotics-team-2022-full.png'
+import me from './assets/robotics/robotics-team-pic-2022-me.png'
+import coders from './assets/robotics/robotics-team-pic-2022-coders.png'
+import full from './assets/robotics/robotics-team-2022-full.png'
 function App() {
   return (
    <div>
@@ -10,20 +10,67 @@ function App() {
     <h2>Volunteering</h2>
     <h3>SJSU Robotics</h3>
     <img src={me} alt="Only Me in Robotics Team" />
-    During Covid I was the only person on the team who was able to program the robot. I spent a lot of time being mentored by a prior president of the club. A lot of the skills I learned from him greatly shaped me to be the programmer I am today and helped me to be able to teach others how to program the robot.
+    <p>During Covid I was the only person on the team who was able to program the robot. I spent a lot of time being mentored by a prior president of the club. A lot of the skills I learned from him greatly shaped me to be the programmer I am today and helped me to be able to teach others how to program the robot.</p>
     <img src={coders} alt="Only Coders in Robotics Team" />
-    Once school opened up, I took on as many recruits as possible to help with the programming of the robot. I interviewed and trained up over 20 people that first year and have probably trained up over 40 people in total since then.
+    <p>Once school opened up, I took on as many recruits as possible to help with the programming of the robot. I interviewed and trained up over 20 people that first year and have probably trained up over 40 people in total since then.</p>
     <img src={full} alt="Full Robotics Team" />
-    Some of the work I did for this club is still some of the most fondest things I've worked on to date. I made many friends and learned a ton not only about programming but also about leadership and how to work with others effectively.
+    <p>Some of the work I did for this club is still some of the most fondest things I've worked on to date. I made many friends and learned a ton not only about programming but also about leadership and how to work with others effectively.</p>
     
     <h3>Cube3</h3>
+    <p>I also helped build out the first cube satellite club at SJSU. This club was officially my first introduction to leadership as I found myself leading their software team. I helped launch the clubs first official website and worked closely with a lot of NASA engineers and scientists from USGS.</p>
+
     <h3>Davis Robotics</h3>
+    <p>After moving to Davis, I got involved with their school's robotics team where I helped build a mini quadruped. The club was sort of flakey but I was able to help teach a few students a few things and made a few friends. This is where I started to feel like I was getting old lol</p>
+
     <h3>HackDavis</h3>
+    <p>I helped mentor for 2 full days. I have a soft spot for hardware projects so I might've helped them more than the other teams. But it felt really badass being able to help so many people for widely different domains whether it be web, hardware, mobile, or gaming. This event was a big realization for me as I started to see how far I had come since transferring to SJSU in 2019. I had learned and grown so much over the short few years without realizing it.</p>
+
     <h3>Progressive Victory</h3>
+    <p>I've always been secretly super engaged in politics since a very early age like around 7 or 8 years old probably. But I never really took much initiative on that front till I found out about this canvasing group. I wasted no time diving in to this organization and started implementing a lot of things I had learn from work and my various projects and clubs. I like to think I got this group headed in the right direction by placing more emphasis on testing and documenting and planning work more effectively. I also experimented a lot with a few of the things I had learned from the various technical books I had read (pair programming, TDD, road mapping, etc) and played around with tools I had not used a ton before but was interested in getting better at like Figma, Cypress, NextJS and really honed in my unit testing skills</p>
+
+    <h2>Hackathons</h2>
+    <h3>Gone with the Fire</h3>
+    <h3>Smart Flask</h3>
+    <h3>Clever Pedal</h3>
+    <h3>Mini Monster Truck</h3>
+    <h3>Smart Clamp</h3>
+
+    <h2>Physical Books</h2>
+    <h3>Refactoring by Martin Fowler</h3>
+    <h3>Test Driven Development by Example by Kent Beck</h3>
+    <h3>Design Patterns by Gang of Four</h3>
+    <h2>Audio Books</h2>
+    <h3>Clean Code by Robert Martin</h3>
+    <h3>Clean Coder by Robert Martin</h3>
+    <h3>Clean Architecture by Robert Martin</h3>
+    <h3>Clean Agile by Robert Martin</h3>
+    <h3>The Pragmatic Programmer by David Thomas, Andrew Hunt</h3>
+    <h3>Staff Engineer by Will Larson</h3>
+    <h3>Lead Without Blame by Diana Larsen, Tricia Broderick</h3>
+    <h3>Retrospectives Antipatterns by Aino Vonge Corry</h3>
+    <h3>Modern Software Engineering by Dave Farley</h3>
+    <h3>Building Microservices by Sam Newman</h3>
+    <h3>Fundamentals of Software Architecture by Mark Richards, Neal Ford</h3>
+    <h3>User Story Mapping by Jeff Patton, Peter Economy</h3>
+    <h3>Designing Data-Intensive Applications by Martin Kleppmann</h3>
 
     <h2>Projects</h2>
-    <p>Project 1</p>
-    <p>Project 2</p>
+    <h3>Gin Rummy</h3>
+    <h3>Cube3 Website</h3>
+    <h3>SupaHotFood</h3>
+    <h3>OnTime</h3>
+    <h3>Laser Turret</h3>
+    <h3>Smart Garden</h3>
+    <h3>US Heatmap</h3>
+    <h3>Fitlog</h3>
+    <h3>Web Serial</h3>
+    <h3>Custom Telemetry Interface</h3>
+    <h3>Astraeus Dashboard</h3>
+    <h3>Splot</h3>
+
+    <h2>Work</h2>
+    <h3>CDK Global</h3>
+    <h3>Genpact</h3>
    </div>
   )
 }
